b7d419342b7b10132035e21ce735752df4abab70
1shadowpy <132641739+1shadowpy@users.noreply.github.com>
2023-06-06 13:01:24 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+# print("hugi bugi kafesiga hush kelibsiz")
+# taomlar = ['kola','pepsi','silver','sunbel']
+# taomlar += ['gril','kabob','donar','lavash','hotdog','tapchikafri','fitter','non']
+# buyirtma= input("taom buyirtma qiling:")
+# buyirtma += input("yana nimadir buirtma qilasizmi")
+# buyirtma += input("ich gani buyirtma qiling:")
+# taomlar2 = []
+# if buyirtma not in taomlar:
+#     taomlar2.append(buyirtma)
+# else:
+#     print(buyirtma)
+# print("buyirtmangiz 2 minutdan kein tayor boladi")
+
+# maxsulotlar = {'kartoshka':3000,'piyoz':7000,'kasnuch':1000,'sabzi':5000}
+# bozorlik  = input('maxsulot kiriting:')
+# bozorlik +=input('yana maxsulot kiriting:')
+# maxsulotlar1 =[]
+# if bozorlik not in maxsulotlar:
+#     print("bida bunday mahsulot yoq")
+# else:
+#     maxsulotlar1.append(bozorlik)
+
+# maxsulotlar = {'kartoshka':3000,'piyoz':7000,'kasnuch':1000,'sabzi':5000}
+# bozorlik  = input('maxsulot kiriting:')
+# bozorlik +=input('yana maxsulot kiriting:')
+# if bozorlik not in maxsulotlar:
+#     print("bizda bunday maxlot yoq")
+# else:
+#     print(f"maxsulotning narxi bu {bozorlik}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
